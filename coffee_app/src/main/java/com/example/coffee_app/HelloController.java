b3e15f33563f1a7e6788c2f63d569975e7406c5e
Miguel-Lameiras <miguel.lameiras@ubi.pt>
2025-08-07 16:17:01 +0100

    implementação da bd apartir do docker

--- coffee_app/src/main/java/com/example/coffee_app/HelloController.java
+++ coffee_app/src/main/java/com/example/coffee_app/HelloController.java
@@ -1,14 +1,12 @@
 package com.example.coffee_app;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String sayHello() {
         return "Hello from Spring Boot with Java 21!";
     }
